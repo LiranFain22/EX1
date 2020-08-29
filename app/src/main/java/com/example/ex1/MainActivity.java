56470b005633febb1f1e36d51d440a99f0fd66f6
LiranFain22 <lirandeveloper@gmail.com>
2020-08-29 13:39:23 +0300

    EX2 work in progress

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -2,16 +2,25 @@ package com.example.ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
+    //-----------------For Version 1----------------//
     private ProgressBar main_PB_spiderman;
     private ProgressBar main_PB_ninja;
 
@@ -37,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
      */
     private int turn = 1;
 
+    //-----------------For Version 2----------------//
+
+    public static final Random RANDOM = new Random();
+
+    private ImageView main_IMG_arrow;
+    private LottieAnimationView main_ANIMATION_flipCoin;
+
+    private Button main_BTN_start;
                 @Override
                 protected void onCreate(Bundle savedInstanceState) {
                     super.onCreate(savedInstanceState);
@@ -44,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
                     findView();
 
-                    startGame();
+                    initializeGame();
                     
                     setListener();
                     
@@ -74,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
                     main_BTN_ninjaAtt1.setOnClickListener(buttonClickListener);
                     main_BTN_ninjaAtt2.setOnClickListener(buttonClickListener);
                     main_BTN_ninjaAtt3.setOnClickListener(buttonClickListener);
+
+                    main_BTN_start.setOnClickListener(buttonClickListener);
     }
 
     private void setListener() {
@@ -82,57 +101,80 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String tag = (String) view.getTag();
 
-                if(tag.equals("SpiderMan_punch")) {
-                    damageAndMessage(SMALL_ATTACK,"SpiderMan Won!",main_PB_ninja);
+                if(tag.equals("start !")){
+                    flipCoin();
                 }
-
-                if(tag.equals("SpiderMan_kick")) {
-                    damageAndMessage(MEDIUM_ATTACK, "SpiderMan Won!",main_PB_ninja);
-                }
-
-                if(tag.equals("SpiderMan_web")) {
-                    damageAndMessage(LARGE_ATTACK, "SpiderMan Won!",main_PB_ninja);
-                }
-
-                if(tag.equals("Ninja_punch")) {
-                    damageAndMessage(SMALL_ATTACK, "Ninja Won!",main_PB_spiderman);
-                }
-
-                if(tag.equals("Ninja_Kick")){
-                    damageAndMessage(MEDIUM_ATTACK, "Ninja Won!",main_PB_spiderman);
-                }
-
-                if(tag.equals("Ninja_star")){
-                    damageAndMessage(LARGE_ATTACK, "Ninja Won!",main_PB_spiderman);
-                }
-                updateProgressBar();
             }
         };
     }
+
+    private void startGame() {
+        turn = RANDOM.nextInt();
+        makeTurn();
+    }
+
+    /**
+     * This method playing the "flip coin" Animation
+     */
+    private void flipCoin() {
+        main_BTN_start.setVisibility(View.INVISIBLE);
+        main_ANIMATION_flipCoin.setVisibility(View.VISIBLE);
+        main_ANIMATION_flipCoin.playAnimation();
+        main_ANIMATION_flipCoin.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+                //main_ANIMATION_flipCoin.setVisibility(View.VISIBLE);
+                Log.d("pttt","Animation START");
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                main_ANIMATION_flipCoin.setVisibility(View.GONE);
+                Log.d("pttt","Animation GONE");
+                startGame();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+    }
+
 
     private void damageAndMessage(int attack, String message, ProgressBar progressBar) {
                     if(turn % 2 != 0){ // SpiderMan attack
                         if((ninjaHealth - attack) > 0){
                             ninjaHealth -= attack;
                             progressBar.setProgress(ninjaHealth);
-                            turns();
+                            turn++;
+                            makeTurn();
                             return;
                         }
                     }else{
                         if((spiderManHealth - attack) > 0){
                             spiderManHealth -= attack;
                             progressBar.setProgress(spiderManHealth);
-                            turns();
+                            turn++;
+                            makeTurn();
                             return;
                         }
                     }
-                    gameOver(message,progressBar);
+                    progressBar.setProgress(0);
+                    main_IMG_arrow.setVisibility(View.INVISIBLE);
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                    gameOver();
     }
 
     /**
-     * The method disable all buttons and show message of the winning player
+     * The method disable all buttons
      */
-    private void gameOver(String message, ProgressBar progressBar) {
+    private void gameOver() {
 
         main_BTN_spidermanAtt1.setEnabled(false);
         main_BTN_spidermanAtt2.setEnabled(false);
@@ -142,11 +184,10 @@ public class MainActivity extends AppCompatActivity {
         main_BTN_ninjaAtt2.setEnabled(false);
         main_BTN_ninjaAtt3.setEnabled(false);
 
-        progressBar.setProgress(0);
-        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+
     }
 
-    private void startGame() {
+    private void initializeGame() {
 
         spiderManHealth = 100;
         ninjaHealth = 100;
@@ -157,33 +198,75 @@ public class MainActivity extends AppCompatActivity {
         main_PB_ninja.getProgressDrawable().setColorFilter(Color.GREEN , PorterDuff.Mode.MULTIPLY);
         main_PB_spiderman.getProgressDrawable().setColorFilter(Color.GREEN , PorterDuff.Mode.MULTIPLY);
 
+        // Calling to this method, before clicking on "start" button
+        gameOver();
 
-        turns();
-
-
+        main_ANIMATION_flipCoin.setVisibility(View.INVISIBLE);
     }
 
     /**
      * This method enable the buttons for the player,
      * depends on the turn
      */
-    private void turns() {
+    private void makeTurn() {
             if(turn % 2 != 0){ // spiderMan turn
+                main_IMG_arrow.setImageResource(R.drawable.left_arrow);
+
                 main_BTN_spidermanAtt1.setEnabled(true);
                 main_BTN_spidermanAtt2.setEnabled(true);
                 main_BTN_spidermanAtt3.setEnabled(true);
                 main_BTN_ninjaAtt1.setEnabled(false);
                 main_BTN_ninjaAtt2.setEnabled(false);
                 main_BTN_ninjaAtt3.setEnabled(false);
+
+                final Handler handler = new Handler();
+                final int delay = 2000; //milliseconds
+                handler.postDelayed(new Runnable(){
+                    public void run(){
+                        randomAttack();
+                        //handler.postDelayed(this, delay);
+                    }
+                }, delay);
             }else{           // ninja turn
-                main_BTN_ninjaAtt1.setEnabled(true);
-                main_BTN_ninjaAtt2.setEnabled(true);
-                main_BTN_ninjaAtt3.setEnabled(true);
+                main_IMG_arrow.setImageResource(R.drawable.right_arrow);
+
                 main_BTN_spidermanAtt1.setEnabled(false);
                 main_BTN_spidermanAtt2.setEnabled(false);
                 main_BTN_spidermanAtt3.setEnabled(false);
+                main_BTN_ninjaAtt1.setEnabled(true);
+                main_BTN_ninjaAtt2.setEnabled(true);
+                main_BTN_ninjaAtt3.setEnabled(true);
+
+                final Handler handler = new Handler();
+                final int delay = 2000; //milliseconds
+                handler.postDelayed(new Runnable(){
+                    public void run(){
+                        randomAttack();
+                        //handler.postDelayed(this, delay);
+                    }
+                }, delay);
             }
-            turn++;
+    }
+
+    private void randomAttack() {
+        int chooseAttack = RANDOM.nextInt(3);
+        if(turn % 2 != 0){ // SpiderMan turn
+            if(chooseAttack == 0)
+                damageAndMessage(SMALL_ATTACK, "SpiderMan Won!", main_PB_ninja);
+            if(chooseAttack == 1)
+                damageAndMessage(MEDIUM_ATTACK, "SpiderMan Won!", main_PB_ninja);
+            if(chooseAttack == 2)
+                damageAndMessage(LARGE_ATTACK, "SpiderMan Won!", main_PB_ninja);
+        }
+        else{
+            if(chooseAttack == 0)
+                damageAndMessage(SMALL_ATTACK, "Ninja Won!", main_PB_spiderman);
+            if(chooseAttack == 1)
+                damageAndMessage(MEDIUM_ATTACK, "Ninja Won!", main_PB_spiderman);
+            if(chooseAttack == 2)
+                damageAndMessage(LARGE_ATTACK, "Ninja Won!", main_PB_spiderman);
+        }
+        updateProgressBar();
     }
 
     private void findView() {
@@ -200,5 +283,14 @@ public class MainActivity extends AppCompatActivity {
         main_BTN_ninjaAtt1 = findViewById(R.id.MainActivity_Btn_Att1_Ninja);
         main_BTN_ninjaAtt2 = findViewById(R.id.MainActivity_Btn_Att2_Ninja);
         main_BTN_ninjaAtt3 = findViewById(R.id.MainActivity_Btn_Att3_Ninja);
+
+        //------------------- Button for Start -------------------------//
+        main_BTN_start = findViewById(R.id.MainActivity_Btn_start);
+
+        //------------------- Arrow left and right -------------------------//
+        main_IMG_arrow = (ImageView)findViewById(R.id.MainActivity_IMG_arrow);
+
+        //------------------- Flip coin Animation -------------------------//
+        main_ANIMATION_flipCoin = findViewById(R.id.MainActivity_ANIMATION_flipCoin);
     }
 }
