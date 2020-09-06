4bf9ef7579115037c3c5031453d134c1e985af23
LiranFain22 <lirandeveloper@gmail.com>
2020-09-06 13:41:39 +0300

    EX2 working in progress

--- app/src/main/java/com/example/ex1/Result_Activity.java
+++ app/src/main/java/com/example/ex1/Result_Activity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,7 +12,9 @@ public class Result_Activity extends AppCompatActivity {
 
     private TextView RESULT_LBL_winnerName;
     private TextView RESULT_LBL_numberOfMoves;
-    private TextView RESULT_LBL_timeCounter;
+
+    private Button RESULT_BTN_menu;
+    private Button RESULT_BTN_top10;
 
 
     @Override
@@ -26,11 +29,7 @@ public class Result_Activity extends AppCompatActivity {
         RESULT_LBL_winnerName.setText("" + i.getExtras().getString("winner"));
         RESULT_LBL_numberOfMoves.setText("" + i.getExtras().getInt("movesCounter"));
 
-        /*
-        TODO -
-            1. add two buttons: "menu" and "Top10"
-            2. make "top10" fragment
-         */
+        // TODO: 06/09/2020 conneting "menu" button
     }
 
 
@@ -39,6 +38,9 @@ public class Result_Activity extends AppCompatActivity {
         //------------------- Results -------------------------//
         RESULT_LBL_winnerName = (TextView) findViewById(R.id.RESULT_LBL_playerName);
         RESULT_LBL_numberOfMoves = (TextView) findViewById(R.id.RESULT_LBL_numberOfMoves);
-        RESULT_LBL_timeCounter = (TextView) findViewById(R.id.RESULT_LBL_timeCounter);
+
+        //------------------- Buttons -------------------------//
+        RESULT_BTN_menu = findViewById(R.id.RESULT_BTN_menu);
+        RESULT_BTN_top10 = findViewById(R.id.RESULT_BTN_Top10);
     }
 }
